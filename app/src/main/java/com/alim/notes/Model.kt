1c83db3ac7856fcfcccaec346319bf65f2b91462
Alimzhan-MacBook <alimzhan.alimkhanov@gmail.com>
2020-02-06 18:08:57 +0600

    первый коммит

---
+++ app/src/main/java/com/alim/notes/Model.kt
@@ -0,0 +1,35 @@
+package com.alim.notes
+
+import android.accounts.AuthenticatorDescription
+
+
+data class Task @JvmOverloads constructor(
+   var title: String,
+   val todos: MutableList<Todo> = mutableListOf(),
+   var tags:  Tag? = null
+)
+
+
+data class Todo(
+   var description: String,
+   var isComplete: Boolean
+)
+
+
+data class Note(
+   var description: String,
+   var tag: Tag? = null
+)
+
+data class Tag(
+   val name: String,
+   val colourResId: Int
+)
+
+data class Control(
+
+        private var type: String? = null,
+        private var text: String? = null,
+        private var action: String? = null
+)
+
